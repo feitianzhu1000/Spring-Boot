c0011156826a251dbe2fa1fdccf8f7f03781d287
feitianzhu1000 <934956990@qq.com>
2023-01-21 19:02:34 +0800

    ceshi2

--- SpringBootDemo/SpringBootDemo/src/main/java/com/gzb/SpringBootDemo/controller/HelloController.java
+++ SpringBootDemo/SpringBootDemo/src/main/java/com/gzb/SpringBootDemo/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.gzb.SpringBootDemo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/demo/")
@@ -15,4 +17,18 @@ public class HelloController {
 	}
 	
 	
+	@RequestMapping("/index")
+	public ModelAndView  index(Model map) {
+		map.addAttribute("msg", "后台数据。。。");
+		//return "index";
+		return new ModelAndView("index");
+		
+	}
+	
+	@RequestMapping("center")
+	public ModelAndView center() {
+		return new ModelAndView("center");
+		//return "freemaker/center/center";
+		//return new ModelAndView("freemaker/center/center");
+	}
 }
---
+++ SpringBootDemo/SpringBootDemo/src/main/java/com/gzb/SpringBootDemo/mapper/TbUserMapper.java
@@ -0,0 +1,7 @@
+package com.gzb.SpringBootDemo.mapper;
+
+import com.gzb.SpringBootDemo.po.TbUser;
+import com.gzb.SpringBootDemo.utils.MyMapper;
+
+public interface TbUserMapper extends MyMapper<TbUser> {
+}

738fb08f0e35f36b1d32953c41b9631108745655
feitianzhu1000 <934956990@qq.com>
2023-01-28 08:39:47 +0800

    ceshi

--- UserDemo/src/main/java/com/gzb/UserDemo/UserDemoApplication.java
+++ UserDemo/src/main/java/com/gzb/UserDemo/UserDemoApplication.java
@@ -3,12 +3,16 @@ package com.gzb.UserDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.gzb.UserDemo.dao")
 @ComponentScan(basePackages= {"com.gzb.UserDemo"})
+
+//打开定时任务
+@EnableScheduling
 public class UserDemoApplication {
 
 	public static void main(String[] args) {

8baf916d386526563f0332ac8a8f7bd30fc5c735
feitianzhu1000 <934956990@qq.com>
2025-10-18 11:42:23 +0800

    添加第一个测试项目到github

---
+++ 20251018/src/HelloDemo.java
@@ -0,0 +1,11 @@
+/**
+ * @author: dwt
+ * @Date:2025/10/18 11:35
+ * @Description:
+ * @Version: 1.0
+ */
+public class HelloDemo {
+    public static void main(String[] args){
+        System.out.println("Hello,World!");
+    }
+}

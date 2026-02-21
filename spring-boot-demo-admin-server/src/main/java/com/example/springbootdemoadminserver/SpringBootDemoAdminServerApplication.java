15ee4b8ad41f84926d4c1e7e2e43b4880d765f9a
feitianzhu1000 <934956990@qq.com>
2026-02-21 16:02:12 +0800

    本demo主要演示了如何搭建一个 Spring Boot Admin 的服务端项目，可视化展示自己客户端项目的运行状态。

---
+++ spring-boot-demo-admin-server/src/main/java/com/example/springbootdemoadminserver/SpringBootDemoAdminServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootdemoadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableAdminServer //启用Admin Server
+public class SpringBootDemoAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoAdminServerApplication.class, args);
+    }
+
+}
